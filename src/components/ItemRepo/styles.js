07697910a82fd3de8879d37c03a8a34e0a1f72ba
Charles <charlesbruno2010@hotmail.com>
2024-07-24 22:10:00 -0300

    feat wiki-git

---
+++ src/components/ItemRepo/styles.js
@@ -0,0 +1,34 @@
+import styled from "styled-components";
+
+export const ItemContainer = styled.div`
+    width: 80%;
+
+    h3{
+        font-size: 32px;
+        color: #FAFAFA;
+    }
+
+    p{
+        font-size: 16px;
+        color: #FAFAFA60;
+        margin-bottom: 10px;
+    }
+
+    hr{
+        color: #FAFAFA60;
+        margin: 20px 0;
+    }
+
+    button{
+        color: #FF0000;
+        margin-top: 10px;
+        font-size: 15px;
+        background: transparent;
+        border: none;
+    }
+    button:hover{
+        color: #FF000060;
+        cursor: pointer;
+    }
+
+`
